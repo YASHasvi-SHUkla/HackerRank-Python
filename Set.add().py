c34b6c79ce43e0298e6e6028ce81ded110924a10
YASHasvi SHUkla <72089033+YASHasvi-SHUkla@users.noreply.github.com>
2021-08-28 16:51:15 +0530

    Create Set.add().py
    If we want to add a single element to an existing set, we can use the .add() operation.
    It adds the element to the set and returns 'None'.
    
    Task
    Apply your knowledge of the .add() operation to help your friend Rupal.
    
    Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of  country stamps.
    
    Find the total number of distinct country stamps.
    
    Input Format
    
    The first line contains an integer , the total number of country stamps.
    The next  lines contains the name of the country where the stamp is from.
    Constraints
    
    
    Output Format
    
    Output the total number of distinct country stamps on a single line.
    
    Sample Input
    7
    UK
    China
    USA
    France
    New Zealand
    UK
    France 
    Sample Output
    5
    
    Explanation
    UK and France repeat twice. Hence, the total number of distinct country stamps is  (five).

---
+++ Set.add().py
@@ -0,0 +1,10 @@
+
+
+n = int(input())   # taking input for the number of country stamps
+
+mikey = set()   #  creating an empty set
+for i in range(0,n):
+    draken = input()
+    mikey.add(draken)   #  adding elements in the set one by one
+print(len(mikey))
+
