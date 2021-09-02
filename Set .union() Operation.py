ce83dc9c3f8bc469cfa11a5d1d28dd517e462686
YASHasvi SHUkla <72089033+YASHasvi-SHUkla@users.noreply.github.com>
2021-09-02 16:25:24 +0530

    Create Set .union() Operation.py
    The .union() operator returns the union of a set and the set of elements in an iterable.
    Sometimes, the | operator is used in place of .union() operator, but it operates only on the set of elements in set.
    Set is immutable to the .union() operation (or | operation).
    
    Task
    The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, some have subscribed to only French and some have subscribed to both newspapers.
    
    You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper. The same student could be in both sets. Your task is to find the total number of students who have subscribed to at least one newspaper.
    
    Input Format
    
    The first line contains an integer, , the number of students who have subscribed to the English newspaper.
    The second line contains  space separated roll numbers of those students.
    The third line contains , the number of students who have subscribed to the French newspaper.
    The fourth line contains  space separated roll numbers of those students.
    
    
    Output Format
    
    Output the total number of students who have at least one subscription.
    
    Sample Input
    
    9
    1 2 3 4 5 6 7 8 9
    9
    10 1 2 3 11 21 55 6 8
    Sample Output
    
    13

---
+++ Set .union() Operation.py
@@ -0,0 +1,10 @@
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+m = int(input())
+n = set(map(int,input().split()))
+
+x = int(input())
+y = set(map(int,input().split()))
+
+draken = n.union(y)
+print(len(draken))
